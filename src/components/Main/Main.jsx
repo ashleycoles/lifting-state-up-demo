6fbe064ccccf9423ab506e1c88a00b8421835dff
Ashley Coles <ashley@acoles.co.uk>
2023-10-25 11:58:52 +0100

    Folder structure

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,13 @@
+import Sidebar from "./Sidebar/Sidebar"
+
+function Main() {
+    return (
+        <main>
+            <Sidebar />
+
+            <h1>Main content</h1>
+        </main>
+    )
+}
+
+export default Main
